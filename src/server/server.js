0d74e23c73a5d2fa2aa00cb82a1ea4209185d32b
Steven van der Merwe <stevenvdmerwe@gmail.com>
2017-01-30 21:58:01 +0200

    Change to port 80

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ const server = new Hapi.Server();
 const routeConfigs = require('./route-loader.js');
 
 
-server.connection({port: 3000, host :'localhost'});
+server.connection({port: 80, host :'localhost'});
 
 // /docs
 server.register([require('vision'), require('inert'), { register: require('lout') }], function(err) {
